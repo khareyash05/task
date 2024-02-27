4806291bed5092a88dbcfee97a9b1d83238762ec
Yash <yash2010118@akgec.ac.in>
2024-02-27 17:43:48 +0530

    minor check update

--- src/db/conn.ts
+++ src/db/conn.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import * as postgres from 'postgres'
 
+// Though public now, I should put it as environment variable using dotenv
 const connectionString = 'postgres://postgres.olgcpzzcpqyxezbuibba:Yashkhare@05@aws-0-ap-south-1.pooler.supabase.com:5432/postgres'
 
 const client = postgres(connectionString)
